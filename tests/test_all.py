ea871b1c91083fafe84ef186cea43374a6ac6f1b
Johann Goltz <johann.goltz@campus.tu-berlin.de>
2020-07-08 17:42:01 +0200

    reference test data by absolute path

--- tests/test_all.py
+++ tests/test_all.py
@@ -66,40 +66,42 @@ class MyTestCase(unittest.TestCase):
 
     @file_data("data/cvdi.yml")
     def test_yml(self, fitfile_path, expected):
-        data, key_mapping = self._preprocess_fitfile(fitfile_path)
+        data, key_mapping = _preprocess_fitfile(os.path.join(get_script_directory(), fitfile_path))
 
         result = do_fancy_things(data, key_mapping)
         self.assertAlmostEqual(result, expected)
 
-    def _preprocess_fitfile(self, file_path):
-        ff = FitFile(file_path)
-        data = []
-        key_mapping = {
-            "position_lat": "Latitude",
-            "position_long": "Longitude",
-            "heart_rate": "Heading",
-            "enhanced_speed": "Speed",
-            "timestamp": "Gentime"
-        }
-        for record in ff.get_messages('record'):
-            record_dict = {metric.name: (metric.value if metric.name != "timestamp" else metric.raw_value) for
-                           metric in record}
-            if all(required_key in record_dict.keys() for required_key in key_mapping):
-                record_dict.update({
-                    'activityId': 7,
-                    'type': 'fitfile_upload',
-                    "position_lat": self.semicircles_to_degrees(
-                        record_dict["position_lat"]),
-                    "position_long": self.semicircles_to_degrees(
-                        record_dict["position_long"])
-                })
-                data.append(record_dict)
-            else:
-                print("skipping")
-        return data, key_mapping
 
-    def semicircles_to_degrees(self, semicircles):
-        return semicircles * 180 / 2 ** 31
+def _preprocess_fitfile(file_path):
+    ff = FitFile(file_path)
+    data = []
+    key_mapping = {
+        "position_lat": "Latitude",
+        "position_long": "Longitude",
+        "heart_rate": "Heading",
+        "enhanced_speed": "Speed",
+        "timestamp": "Gentime"
+    }
+    for record in ff.get_messages('record'):
+        record_dict = {metric.name: (metric.value if metric.name != "timestamp" else metric.raw_value) for
+                       metric in record}
+        if all(required_key in record_dict.keys() for required_key in key_mapping):
+            record_dict.update({
+                'activityId': 7,
+                'type': 'fitfile_upload',
+                "position_lat": semicircles_to_degrees(
+                    record_dict["position_lat"]),
+                "position_long": semicircles_to_degrees(
+                    record_dict["position_long"])
+            })
+            data.append(record_dict)
+        else:
+            print("skipping")
+    return data, key_mapping
+
+
+def semicircles_to_degrees(semicircles):
+    return semicircles * 180 / 2 ** 31
 
 
 def get_script_directory():
