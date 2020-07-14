aadb2c520238b88f2cdd467e0ac4eae07a148d78
Johann Goltz <johann.goltz@campus.tu-berlin.de>
2020-07-14 18:50:35 +0200

    more sophisticated k-anonymity example

--- config_creation/kanon_cn_config.py
+++ config_creation/kanon_cn_config.py
@@ -5,3 +5,11 @@ cn_config = {
     "start_longitude": ("quasi", OrderHierarchy("interval", 1, 2, 4)),
     "external_id": ("identifying", None)
 }
+longlathierarchy = ("quasi", OrderHierarchy("interval", 1, 2, 4))
+cn_config = {
+    "start_lat": longlathierarchy,
+    "start_long": longlathierarchy,
+    "username": ("identifying", None),
+    "end_long": longlathierarchy,
+    "end_lat": longlathierarchy
+}
